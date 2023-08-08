f6dcee3091a2b6231947a2b857ff079e945ec181
Jpost <Jpost2010@Gmail.com>
2023-08-07 21:49:28 -0400

    added functionality to the progress bar including color as you complete exercises.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var progress = 0;
+
 //shows the modal window
 function openModal(){
     $('#active-modal').addClass('is-active');
@@ -8,6 +10,30 @@ function closeModal(){
     $('#active-modal').removeClass('is-active');
 };
 
+function progressUp(){
+    if(progress == 0){
+        $('.progress').attr('value', '33');
+        progress = progress + 33;
+        console.log(progress);
+    }else if(progress == 33){
+        $('.progress').attr('value', '66');
+        $('.progress').removeClass('is-danger');
+        $('.progress').addClass('is-warning');
+
+        progress = progress + 33;
+        console.log(progress);
+    }else if(progress == 66){
+        $('.progress').attr('value', '100');
+        $('.progress').removeClass('is-warning');
+        $('.progress').addClass('is-success');
+        progress = progress + 34;
+        console.log(progress);
+    }else{
+        $('.progress').addClass('is-hidden');
+        $('#progress-title').text('Workout Complete!')
+    }
+}
+
 //click button to open modal
 $('#btn').on('click', function(){
     openModal();
@@ -16,7 +42,8 @@ $('#btn').on('click', function(){
 //button to move onto the next exercise
 //currently set to close modal
 $('#nextBtn').on('click', function(){
-    closeModal();
+
+    progressUp();
 });
 
 //button to skip to the next exercise without counting as 'completed'
